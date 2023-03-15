26b21b95ee830362e077f8c05c893e23b545c0a8
Kerimhan05 <ramazanovkerimhan05@gmail.com>
2023-03-15 23:41:36 +0300

    Инициализация проекта

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus ea, quo
+      eligendi veritatis, sunt temporibus nihil placeat, sed reprehenderit
+      distinctio nam. Libero, facere? Modi laborum eligendi doloremque,
+      consectetur necessitatibus ipsam.
+    </div>
+  );
+}
